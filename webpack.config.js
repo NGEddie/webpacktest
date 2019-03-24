d3f14230b1426cd9e6cddafa0cec041155cd9528
Nigel Gormley <nigel.gormley@gmail.com>
2019-03-24 13:54:13 +0000

    Added forms for changepassword, index, login and resetpassword

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist', 'js'),
-    filename: 'changepassword.js'
+    filename: 'authForm.js'
   },
   module: {
     rules: [
